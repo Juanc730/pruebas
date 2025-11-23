59c661e856357e7e313a89208d4801a93c5190f9
Juanc730 <juancarlosdlcc@gmail.com>
2025-11-23 18:03:13 -0500

    v1.4

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
